d9a9bb840f700aa34065063a2de95bab85b81f16
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2024-10-13 12:36:12 +0200

    feat(front): page card link to driver page

--- apps/front/src/components/cards/CardsListContainer.tsx
+++ apps/front/src/components/cards/CardsListContainer.tsx
@@ -1,10 +1,13 @@
 import { FC, PropsWithChildren } from 'react';
+import { ScrollArea } from '../ui/scroll-area';
 
 export const CardsListContainer: FC<PropsWithChildren> = ({ children }) => (
-  <div
-    id="cards-list-container"
-    className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4"
-  >
-    {children}
-  </div>
+  <ScrollArea>
+    <div
+      id="cards-list-container"
+      className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4"
+    >
+      {children}
+    </div>
+  </ScrollArea>
 );
--- apps/front/src/pages/cards/Card.page.tsx
+++ apps/front/src/pages/cards/Card.page.tsx
@@ -2,6 +2,8 @@ import { Page } from '@/components/nav/Page';
 import { cardRoute } from '@/services/router/router.routes';
 import { useDriverCard } from './cards.api';
 import { DriverCard } from '@/components/cards/drivers/DriverCard';
+import { Link } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
 
 export const CardPage = () => {
   const { id } = cardRoute.useParams();
@@ -16,10 +18,21 @@ export const CardPage = () => {
       </div>
     );
 
+  const { driver } = card;
+
   return (
     <Page padding>
-      <div className="h-full flex justify-center items-center">
-        <DriverCard card={card} />
+      <div className="h-full flex justify-center items-center ">
+        <div className="mx-10">
+          <DriverCard card={card} />
+        </div>
+        <div className="flex-1 h-full ">
+          <Link to={`/drivers/${driver.id}`}>
+            <Button variant="link" className="text-xl font-bold">
+              {driver.fullName}
+            </Button>
+          </Link>
+        </div>
       </div>
     </Page>
   );
