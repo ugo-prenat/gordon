7021584e33548631bf4225129d957a56e2a3b9f9
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2024-10-27 11:44:51 +0100

    feat(front): dark theme as default 🥷

--- apps/front/src/services/store/settings/settings.models.ts
+++ apps/front/src/services/store/settings/settings.models.ts
@@ -1,7 +1,7 @@
 export const SETTINGS_STORAGE_KEY = 'settings';
 
 export type Theme = 'light' | 'dark' | 'system';
-export const DEFAULT_THEME: Theme = 'system';
+export const DEFAULT_THEME: Theme = 'dark';
 
 export type Lang = 'en' | 'fr';
 export const DEFAULT_LANG: Lang = navigator.language.startsWith('fr')
